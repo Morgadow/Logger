a6b2b4edd8d2cbc8d8805b0d3176c50559fb335c
mihau <46446239+Morgadow@users.noreply.github.com>
2019-12-16 13:44:58 +0100

    testing borg patter, every instance loads new settings

--- test.py
+++ test.py
@@ -5,9 +5,27 @@ import os
 
 
 
-logger = Logger(loglvl='DEBUG', addtimestamp=True, suppressloggernotes=False)
-logger.set_debug()
-logger.set_debug()
-logger.set_debug()
+# logger = Logger(loglvl='DEBUG', addtimestamp=True, suppressloggernotes=False, createlogfile=True, logpath=os.path.normpath("C:\Users\QXY5145\Desktop\Python_INI_Skript_V2"))
+logger = Logger(loglvl='DEBUG', addtimestamp=True, suppressloggernotes=False, projectname="INI_TESTING BMU_R18", createlogfile=True, logpath=os.path.normpath("C:\\Users\\QXY5145\\Desktop\\Python_INI_Skript_V2\\logs"))
+# logger = Logger(loglvl='DEBUG', addtimestamp=True, suppressloggernotes=False, createlogfile=True, logpath=os.path.normpath("C:\\Users\\QXY5145\\Desktop\\Python_INI_Skript_V2"))
+# logger = Logger(loglvl='DEBUG', addtimestamp=True, suppressloggernotes=False, createlogfile=True)
 logger.info("ich bin ein Promi")
 logger.warning("holt mich hier raus!")
+logger.set_error()
+logger.warning("HUHU ich bin eine warnung")
+logger.critical("scheise ich bin critical")
+
+
+print("logger1")
+print(logger.get_log_level())
+print(logger._log_file)
+print(logger.create_log_file)
+
+print("logger2")
+logger2 = Logger()
+print(logger2.get_log_level())
+print(logger2._log_file)
+print(logger2.create_log_file)
+
+print("logger1")
+print(logger.get_log_level())
