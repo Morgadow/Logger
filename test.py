f3363952b4506dfca717d51ed8f9245edf8721cf
Simon <simon_sch92@web.de>
2020-01-08 23:12:18 +0100

    Bugfixing, tested logger over multiple packages

--- test.py
+++ test.py
@@ -2,6 +2,7 @@
 
 from logger import Logger
 import os
+import test2
 
 
 # # logger = Logger(loglvl='DEBUG', addtimestamp=True, suppressloggernotes=False, createlogfile=True, logpath=os.path.normpath("C:\Users\QXY5145\Desktop\Python_INI_Skript_V2"))
@@ -32,16 +33,20 @@ import os
 
 
 
-# logger 1
-# logger1 = Logger(level='DEBUG', suppressloggernotes=False)
-logger1 = Logger()
-print("logger1 level " + logger1.get_level()[0])
-print("suppresloggernotes logger1: " + str(logger1.suppress_logger_notes))
-# logger2 = Logger(level="ERROR")
-# logger2 = Logger(level="ERROR")
-logger3 = Logger(suppressloggernotes=False)
-logger2 = Logger(level="INFO")
-# logger2 = Logger()
-print("logger1 level " + logger1.get_level()[0])
-print("logger2 level " + logger2.get_level()[0])
+# # logger 1
+# # logger1 = Logger(level='DEBUG', suppressloggernotes=False)
+# logger1 = Logger()
+# print("logger1 level " + logger1.get_level()[0])
+# print("suppresloggernotes logger1: " + str(logger1.suppress_logger_notes))
+# # logger2 = Logger(level="ERROR")
+# # logger2 = Logger(level="ERROR")
+# logger3 = Logger(suppressloggernotes=False)
+# logger2 = Logger(level="INFO")
+# # logger2 = Logger()
+# print("logger1 level " + logger1.get_level()[0])
+# print("logger2 level " + logger2.get_level()[0])
 
+logger1 = Logger(level='ERROR')
+print("logger1 : " + str(logger1.get_level()[0]))
+test2.call_new_logger()
+print("logger1 : " + str(logger1.get_level()[0]))
